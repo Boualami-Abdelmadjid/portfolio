2de81980d9f400f34b341a5fe4fb80c13448d25c
Boualami ABdelmadjid <majidaveiro@gmail.com>
2022-08-15 03:09:04 +0100

    prelaoder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Main from "./components/Main/Main";
+import spinner from "./spinner.gif";
 
 function App() {
   const [shown, setShown] = useState("Home");
+  const [loaded, setLoaded] = useState(false);
+
+  const loadDocument = () => {
+    if (document.readyState === "complete") {
+      setLoaded(true);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("readystatechange", loadDocument);
+    return () => {
+      document.removeEventListener("readystatechange", loadDocument);
+    };
+  }, []);
   const stateHandler = (state) => {
     document.title = state;
     setShown(state);
   };
   return (
     <div className="App">
+      <div className={`preloader ${loaded ? "hidden" : ""}`}>
+        <img src={spinner} alt="" />
+      </div>
       <Nav onStateChange={stateHandler} />
       <Main shown={shown} />
     </div>
