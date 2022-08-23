68684359c4181472053b22adf8aa3ef5a8571085
Boualami ABdelmadjid <majidaveiro@gmail.com>
2022-08-23 14:06:03 +0100

    lazy loading

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import styles from "./Main.module.css";
+import spinner from "../../spinner.gif";
 
 import Home from "../Home/Home";
-import Skills from "../Skills/Skills";
-import Certs from "../Certs/Certs";
-import About from "../About/About";
+
+const Skills = React.lazy(() => import("../Skills/Skills"));
+const Certs = React.lazy(() => import("../Certs/Certs"));
+const About = React.lazy(() => import("../About/About"));
 
 export default function Main(props) {
   return (
     <div className={styles.main}>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/certs" element={<Certs />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div>
+            <img src={spinner} alt="Loading..." />
+          </div>
+        }
+      >
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/certs" element={<Certs />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
