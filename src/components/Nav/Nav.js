0ae700f6d12e7a0cc72510c78562d8a73f53da11
Boualami ABdelmadjid <majidaveiro@gmail.com>
2022-10-07 13:38:02 +0100

    styled active navlink

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,6 +4,8 @@ import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   const [navShown, setNavShown] = useState(false);
+  const [title, setTitle] = useState(document.title);
+  document.title = title;
   const toggleNavHandler = () => {
     setNavShown((prev) => !prev);
   };
@@ -11,10 +13,12 @@ export default function Nav() {
   const linkedInHandler = () =>
     window.open("https://www.linkedin.com/in/abdelmadjid-boualami/", "_blank");
   let activeStyle = {
-    width: "150% ",
-    backgroundColor: "grey",
-    marginLeft: "-5rem",
-    paddingInline: "5rem",
+    // borderLeft: "2px solid whitesmoke",
+    boxShadow: "-5px 0px 0px 0px #c770f0",
+    paddingLeft: "0.5rem",
+    // width: "150% ",
+    marginLeft: "-0.5rem",
+    // paddingInline: "5rem",
   };
   return (
     <nav
@@ -28,37 +32,65 @@ export default function Nav() {
         >
           <ion-icon id={styles.menu} name="menu-outline" />
         </li>
-        <li className={styles.navButton} onClick={closeNav}>
+        <li
+          className={styles.navButton}
+          onClick={() => {
+            closeNav();
+            setTitle("Majid's portfolio");
+          }}
+        >
           <NavLink
             to="/"
             style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            title="home"
           >
             <ion-icon name="home-outline" />{" "}
             <span className={styles.span}> Home</span>
           </NavLink>
         </li>
-        <li className={styles.navButton} onClick={closeNav}>
+        <li
+          className={styles.navButton}
+          onClick={() => {
+            closeNav();
+            setTitle("Skills");
+          }}
+        >
           <NavLink
             to="skills"
             style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            title="Skills"
           >
             <ion-icon name="code-outline" />{" "}
             <span className={styles.span}>Skills</span>
           </NavLink>
         </li>
-        <li className={styles.navButton} onClick={closeNav}>
+        <li
+          className={styles.navButton}
+          onClick={() => {
+            closeNav();
+            setTitle("Certifications");
+          }}
+        >
           <NavLink
             to="certs"
             style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            title="Certs"
           >
             <ion-icon name="document-outline" />{" "}
             <span className={styles.span}>Certs</span>
           </NavLink>
         </li>
-        <li className={styles.navButton} onClick={closeNav}>
+        <li
+          className={styles.navButton}
+          onClick={() => {
+            closeNav();
+            setTitle("About me");
+          }}
+        >
           <NavLink
             to="about"
             style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            title="About"
           >
             <ion-icon name="information-circle-outline" />{" "}
             <span className={styles.span}>About</span>
